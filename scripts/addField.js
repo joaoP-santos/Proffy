38b087e31d50ec4f7743f3bf41e3a4b035efd465
João Pedro Santos <potatohunter47@gmail.com>
2020-08-06 08:55:05 -0300

    Added "addField" script

---
+++ scripts/addField.js
@@ -0,0 +1,14 @@
+document.querySelector('#addtime')
+.addEventListener('click', cloneField)
+
+function cloneField() {
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    fields.forEach(function (field) {
+        field.value = ''
+    })
+    
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
